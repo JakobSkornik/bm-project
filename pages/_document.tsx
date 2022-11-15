465492a63842d1faf65ead1a0beb7cdf69d1543f
JakobSkornik <skornikjakobs@gmail.com>
2022-11-15 20:48:53 +0100

    p5 modz

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <title>BM Project</title>
+      <meta name="description" content="Project for course BM" />
+      <link rel="icon" href="/favicon.svg" />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
