bd5d2a42fe96cf6853528f9e5f416d920f8543ab
JakobSkornik <skornikjakobs@gmail.com>
2022-11-18 03:50:35 +0100

    Added more brownish fade

--- components/Background.tsx
+++ components/Background.tsx
@@ -6,7 +6,7 @@ const sx = {
     width: '100vw',
     height: '100vh',
     background:
-      'radial-gradient(circle, rgba(235,235,235,1) 0%, rgba(196,196,196,1) 59%, rgba(163,163,163,1) 100%)',
+      'radial-gradient(circle, rgba(244,240,232,1) 0%, rgba(196,196,196,1) 59%, rgba(169,163,157,1) 100%)',
   },
 }
 
